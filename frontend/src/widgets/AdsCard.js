381ebd1e3cb0b8c168c4b530f01a68a6266b205d
mohitleo03 <79054705+mohitleo03@users.noreply.github.com>
2022-11-07 20:18:47 +0530

    update

--- frontend/src/widgets/AdsCard.js
+++ frontend/src/widgets/AdsCard.js
@@ -3,51 +3,55 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
 
 export default function AdsCard(props) {
-  let {
-    imageUrl,
-    companyName,
-    headline,
-    primaryText,
-    companyUrl,
-  } = props;
+  let { imageUrl, companyName, headline, primaryText, companyUrl } = props;
 
   let arr = imageUrl.split("/");
-      let potentialIndex = -1;
-      arr.forEach((v, i)=>{
-        if(v == "d"){
-          potentialIndex = i+1;
-        }
-      })
+  let potentialIndex = -1;
+  arr.forEach((v, i) => {
+    if (v == "d") {
+      potentialIndex = i + 1;
+    }
+  });
 
-      if(potentialIndex!=-1 && potentialIndex<arr.length && potentialIndex+1<arr.length && arr[potentialIndex+1].includes("view")){
-          imageUrl = "https://drive.google.com/uc?export=view&id="+arr[potentialIndex];
-      }
+  if (
+    potentialIndex != -1 &&
+    potentialIndex < arr.length &&
+    potentialIndex + 1 < arr.length &&
+    arr[potentialIndex + 1].includes("view")
+  ) {
+    imageUrl =
+      "https://drive.google.com/uc?export=view&id=" + arr[potentialIndex];
+  }
+  function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
 
   return (
-    <Card sx={{ maxWidth: 345 ,margin:2}}>
+    <Card sx={{ maxWidth: 345, margin: 2 }}>
       <CardMedia
         component="img"
         alt="green iguana"
         height="140"
-        image={ imageUrl }
+        image={imageUrl}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          { headline }
+          {headline}
         </Typography>
         <Typography gutterBottom variant="h6" component="div">
-          { companyName }
+          {companyName}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          { primaryText }
+          {primaryText}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button onClick={()=>{openInNewTab(companyUrl)}}></Button>
       </CardActions>
     </Card>
   );
